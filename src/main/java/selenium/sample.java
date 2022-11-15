342c90f297b90665ac9c8d24b7af6136034c8f30
Makoto Shinozuka <makoto.shinozuka@rakus-partners.co.jp>
2022-11-15 14:48:11 +0900

    enshu2

--- src/main/java/selenium/sample.java
+++ src/main/java/selenium/sample.java
@@ -1,5 +1,8 @@
 package selenium;
 
+import java.util.List;
+import java.util.ListIterator;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -13,11 +16,28 @@ public class sample {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.co.jp");
-		WebElement element = driver.findElement(By.cssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"));
-		element.sendKeys("Selenium");
-		element.sendKeys(Keys.ENTER);
-		System.out.println(driver.getCurrentUrl());
+		driver.get("https://rakuplus.jp/");
+		driver.findElement(By.cssSelector("#user_login")).sendKeys("");
+		driver.findElement(By.cssSelector("#user_pass")).sendKeys("");
+		driver.findElement(By.cssSelector("#wp-submit")).click();
+		
+		driver.get("https://rakuplus.jp/archives/11203");
+		List<WebElement> divElemList = driver.findElements(By.cssSelector("section.entry-content > div"));	
+		int count = 0;
+		for(WebElement elem : divElemList) {
+			if(count > 0) {
+				WebElement imgElem = elem.findElement(By.tagName("img"));
+				String url = imgElem.getAttribute("src");
+				System.out.println(url);
+				WebElement spanElem = elem.findElement(By.cssSelector("span.big"));
+				System.out.println(spanElem.getText());
+				WebElement pElem = elem.findElement(By.cssSelector("#\\31  > div.is-layout-flow.wp-block-column.is-vertically-aligned-top.visual > p"));
+				System.out.println(pElem.getText());
+			}
+			
+			System.out.println("-----------------------------");
+			count++;
+		}
 	}
-
 }
+
